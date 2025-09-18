8996795a87d3fbbc1e6af0eae093cef8836a0b50
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2025-09-18 14:05:03 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -1,3 +1,10 @@
+import math
+
+from pyrogram.types import InlineKeyboardButton
+
+from BrandrdXMusic.utils.formatters import time_to_seconds
+
+
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
         [
